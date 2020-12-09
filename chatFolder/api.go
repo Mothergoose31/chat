97f76ecf8f1c921a86e02527308aa33caf491dcc
Nata <JosephAlvarengaBeech@protonmail.com>
2020-12-09 13:57:21 -0800

    oAuth

--- chatFolder/api.go
+++ chatFolder/api.go
@@ -24,3 +24,36 @@ func initApi(url, key string) {
 		key: key,
 	}
 }
+
+
+func (a *Api) getUserFromAuthToken(tok string) ([]byte, error) {
+	if !cookievalid.MatchString(tok) {
+		return nil, fmt.Errorf("api: auth token cookie invalid %s", tok)
+	}
+
+	endpoint := a.url + "/auth"
+	resp, err := http.PostForm(endpoint, url.Values{
+		"authtoken":  {tok},
+		"privatekey": {a.key},
+	})
+
+	if resp != nil && resp.Body != nil {
+		defer resp.Body.Close()
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("api: auth token invalid: %s, response code: %d", tok, resp.StatusCode)
+	}
+
+	ret, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return ret, nil
+}
+
