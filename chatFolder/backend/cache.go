ee23628813afecdb5ebd4bc28e7455a62d3b5a0b
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-12-14 11:41:36 -0800

    -

--- chatFolder/backend/cache.go
+++ chatFolder/backend/cache.go
@@ -1,3 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/tideland/golib/redis"
+)
+
+var (
+	rds               *redis.Database
+	rdsCircularBuffer string
+	rdsGetIPCache     string
+	rdsSetIPCache     string
+)
+
+// how many log lines to buffer for the scrollback
+const CHATLOGLINES = 150
+
+func redisGetConn() *redis.Connection {
+again:
+	conn, err := rds.Connection()
+	if err != nil {
+		D("Error getting a redis connection", err)
+		if conn != nil {
+			conn.Return()
+		}
+		time.Sleep(500 * time.Millisecond)
+		goto again
+	}
+
+	return conn
+}
+
+
+
 func (db *database) getUser(nick string) (Userid, bool) {
 
 	stmt := db.getStatement("getUser", `
