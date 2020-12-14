170a11dac3de3c0f98b953b1972ee158ce910ac1
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-12-14 13:33:30 -0800

    progress

--- chatFolder/backend/cache.go
+++ chatFolder/backend/cache.go
@@ -34,3 +34,45 @@ again:
 
 
 
+func initRedis(addr string, db int64, pw string) {
+	var err error
+	rds, err = redis.Open(
+		redis.TcpConnection(addr, 1*time.Second),
+		redis.Index(int(db), pw),
+		redis.PoolSize(50),
+	)
+	if err != nil {
+		F("Error making the redis pool", err)
+	}
+
+	conn := redisGetConn()
+	defer conn.Return()
+
+	rdsCircularBuffer, err = conn.DoString("SCRIPT", "LOAD", `
+		local key = KEYS[1]
+		local maxlength = tonumber(ARGV[1])
+		local payload = ARGV[2]
+		if not key then
+			return {err = "INVALID KEY"}
+		end
+		if not payload then
+			return {err = "INVALID PAYLOAD"}
+		end
+		if not maxlength then
+			return {err = "INVALID MAXLENGTH"}
+		end
+		-- push the payload onto the end
+		redis.call("RPUSH", key, payload)
+		local delcount = 0
+		-- get rid of excess lines from the front
+		local numlines = redis.call("LLEN", key)
+		for _ = numlines - 1, maxlength, -1 do
+			redis.call("LPOP", key)
+			delcount = delcount + 1
+		end
+		return delcount
+	`)
+	if err != nil {
+		F("Circular buffer script loading error", err)
+	}
+
