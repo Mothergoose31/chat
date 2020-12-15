fb167963a865d32d947d14828d8f86fb2969bdd7
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-12-15 12:00:40 -0800

    ban user

--- chatFolder/backend/ban_user.go
+++ chatFolder/backend/ban_user.go
@@ -17,3 +17,49 @@ type Bans struct {
 	userips  map[Userid][]string
 	iplock   sync.RWMutex // protects both ips/userips
 }
+var (
+	ipv6mask = net.CIDRMask(64, 128)
+	bans     = Bans{
+		make(map[Userid]time.Time),
+		sync.RWMutex{},
+		make(map[string]time.Time),
+		make(map[Userid][]string),
+		sync.RWMutex{},
+	}
+)
+
+func getMaskedIP(s string) string {
+	ip := net.ParseIP(s)
+	if ip.To4() == nil {
+		return ip.Mask(ipv6mask).String()
+	} else {
+		return s
+	}
+}
+
+func initBans(redisdb int64) {
+	go ban_user.run(redisdb)
+}
+
+func (b *Bans) run(redisdb int64) {
+	b.loadActive()
+
+	go b.runRefresh(redisdb)
+	go b.runUnban(redisdb)
+
+	t := time.NewTicker(time.Minute)
+
+	for {
+		select {
+		case <-t.C:
+			b.clean()
+		}
+	}
+}
+
+func (b *Bans) runRefresh(redisdb int64) {
+	setupRedisSubscription("refreshbans", redisdb, func(result *redis.PublishedValue) {
+		D("Refreshing bans")
+		b.loadActive()
+	})
+}
