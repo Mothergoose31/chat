d2a16a7dbd860fb671963557eced172cd4cccddb
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-12-16 12:31:51 -0800

    regex

--- chatFolder/backend/connection.go
+++ chatFolder/backend/connection.go
@@ -12,3 +12,31 @@ import (
 
 	"github.com/gorilla/websocket"
 )
+
+
+
+
+// regexp to detect three or more consecutive characters intended to be combined
+// with another char (like accents, diacritics), if there are more than 5
+
+var invalidmessage = regexp.MustCompile(`\p{M}{5,}|[\p{Zl}\p{Zp}\x{202f}\x{00a0}]`)
+
+
+
+type Connection struct {
+	socket         *websocket.Conn
+	ip             string
+	send           chan *message
+	sendmarshalled chan *message
+	blocksend      chan *message
+	banned         chan bool
+	stop           chan bool
+	user           *User
+	ping           chan time.Time
+	sync.RWMutex
+}
+
+type SimplifiedUser struct {
+	Nick     string    `json:"nick,omitempty"`
+	Features *[]string `json:"features,omitempty"`
+}
