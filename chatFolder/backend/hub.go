e0993d52ad3371275c40fe8f3940c819594d5e0c
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-12-17 15:44:41 -0800

    hub

--- chatFolder/backend/hub.go
+++ chatFolder/backend/hub.go
@@ -43,3 +43,72 @@ var hub = Hub{
 func initHub() {
 	go hub.run()
 }
+
+
+func (hub *Hub) run() {
+	pinger := time.NewTicker(PINGINTERVAL)
+
+	for {
+		select {
+		case c := <-hub.register:
+			hub.connections[c] = true
+		case c := <-hub.unregister:
+			delete(hub.connections, c)
+		case userid := <-hub.refreshuser:
+			for c, _ := range hub.connections {
+				if c.user != nil && c.user.id == userid {
+					go c.Refresh()
+				}
+			}
+		case userid := <-hub.bans:
+			for c, _ := range hub.connections {
+				if c.user != nil && c.user.id == userid {
+					go c.Banned()
+				}
+			}
+		case stringip := <-hub.ipbans:
+			for c := range hub.connections {
+				if c.ip == stringip {
+					DP("Found connection to ban with ip", stringip, "user", c.user)
+					go c.Banned()
+				}
+			}
+		case d := <-hub.getips:
+			ips := make([]string, 0, 3)
+			for c, _ := range hub.connections {
+				if c.user != nil && c.user.id == d.userid {
+					ips = append(ips, c.ip)
+				}
+			}
+			d.c <- ips
+		case message := <-hub.broadcast:
+			if message.event != "JOIN" && message.event != "QUIT" {
+				cacheChatEvent(message)
+			}
+
+			for c := range hub.connections {
+				if len(c.sendmarshalled) < SENDCHANNELSIZE {
+					c.sendmarshalled <- message
+				}
+			}
+		case p := <-hub.privmsg:
+			for c, _ := range hub.connections {
+				if c.user != nil && c.user.id == p.targetuid {
+					if len(c.sendmarshalled) < SENDCHANNELSIZE {
+						c.sendmarshalled <- &p.message
+					}
+				}
+			}
+		// timeout handling
+		case t := <-pinger.C:
+			for c := range hub.connections {
+				if c.ping != nil && len(c.ping) < 2 {
+					c.ping <- t
+				} else if c.ping != nil {
+					close(c.ping)
+					c.ping = nil
+				}
+			}
+		}
+	}
+}
