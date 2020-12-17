f560f6a794956d834dc6a1f04f56db009a7ccbfd
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-12-17 11:03:09 -0800

    SOCKETS!

--- chatFolder/backend/connection.go
+++ chatFolder/backend/connection.go
@@ -86,3 +86,107 @@ type PrivmsgOut struct {
 	Nick      string `json:"nick,omitempty"`
 	Data      string `json:"data,omitempty"`
 }
+
+
+func newConnection(s *websocket.Conn, user *User, ip string) {
+	c := &Connection{
+		socket:         s,
+		ip:             ip,
+		send:           make(chan *message, SENDCHANNELSIZE),
+		sendmarshalled: make(chan *message, SENDCHANNELSIZE),
+		blocksend:      make(chan *message),
+		banned:         make(chan bool, 8),
+		stop:           make(chan bool),
+		user:           user,
+		ping:           make(chan time.Time, 2),
+		RWMutex:        sync.RWMutex{},
+	}
+
+	go c.writePumpText()
+	c.readPumpText()
+}
+
+func (c *Connection) readPumpText() {
+	defer func() {
+		namescache.disconnect(c.user)
+		c.Quit()
+		c.socket.Close()
+	}()
+
+	c.socket.SetReadLimit(MAXMESSAGESIZE)
+	c.socket.SetReadDeadline(time.Now().Add(READTIMEOUT))
+	c.socket.SetPongHandler(func(string) error {
+		c.socket.SetReadDeadline(time.Now().Add(PINGTIMEOUT))
+		return nil
+	})
+	c.socket.SetPingHandler(func(string) error {
+		c.sendmarshalled <- &message{
+			msgtyp: websocket.PongMessage,
+			event:  "PONG",
+			data:   []byte{},
+		}
+		return nil
+	})
+
+	if c.user != nil {
+		c.rlockUserIfExists()
+		n := atomic.LoadInt32(&c.user.connections)
+		if n > 5 {
+			c.runlockUserIfExists()
+			c.SendError("toomanyconnections")
+			c.stop <- true
+			return
+		}
+		c.runlockUserIfExists()
+	} else {
+		namescache.addConnection()
+	}
+
+	hub.register <- c
+	c.Names()
+	c.Join() // broadcast to the chat that a user has connected
+
+	// Check mute status.
+	muteTimeLeft := mutes.muteTimeLeft(c)
+	if muteTimeLeft > time.Duration(0) {
+		c.EmitBlock("ERR", NewMutedError(muteTimeLeft))
+	}
+
+	for {
+		msgtype, message, err := c.socket.ReadMessage()
+		if err != nil || msgtype == websocket.BinaryMessage {
+			return
+		}
+
+		name, data, err := Unpack(string(message))
+		if err != nil {
+			// invalid protocol message from the client, just ignore it,
+			// disconnect the user
+			return
+		}
+
+		// dispatch
+		switch name {
+		case "MSG":
+			c.OnMsg(data)
+		case "MUTE":
+			c.OnMute(data)
+		case "UNMUTE":
+			c.OnUnmute(data)
+		case "BAN":
+			c.OnBan(data)
+		case "UNBAN":
+			c.OnUnban(data)
+		case "SUBONLY":
+			c.OnSubonly(data)
+		case "PING":
+			c.OnPing(data)
+		case "PONG":
+			c.OnPong(data)
+		case "BROADCAST":
+			c.OnBroadcast(data)
+		case "PRIVMSG":
+			c.OnPrivmsg(data)
+		}
+	}
+}
