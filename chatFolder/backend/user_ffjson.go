b3a7f4eec1ec99a830f4d101c7fc79ea205c8755
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-12-21 12:48:17 -0800

    ffjson

--- chatFolder/backend/user_ffjson.go
+++ chatFolder/backend/user_ffjson.go
@@ -1,3 +1,4 @@
+
 package main
 
 import (
@@ -42,3 +43,140 @@ func (mj *sessionuser) MarshalJSONBuf(buf fflib.EncodingBuffer) error {
 	buf.WriteByte('}')
 	return nil
 }
+const (
+	ffj_t_sessionuserbase = iota
+	ffj_t_sessionuserno_such_key
+
+	ffj_t_sessionuser_Features
+
+	ffj_t_sessionuser_UserId
+
+	ffj_t_sessionuser_Username
+)
+
+var ffj_key_sessionuser_Features = []byte("features")
+
+var ffj_key_sessionuser_UserId = []byte("userId")
+
+var ffj_key_sessionuser_Username = []byte("username")
+
+func (uj *sessionuser) UnmarshalJSON(input []byte) error {
+	fs := fflib.NewFFLexer(input)
+	return uj.UnmarshalJSONFFLexer(fs, fflib.FFParse_map_start)
+}
+
+func (uj *sessionuser) UnmarshalJSONFFLexer(fs *fflib.FFLexer, state fflib.FFParseState) error {
+	var err error = nil
+	currentKey := ffj_t_sessionuserbase
+	_ = currentKey
+	tok := fflib.FFTok_init
+	wantedTok := fflib.FFTok_init
+
+mainparse:
+	for {
+		tok = fs.Scan()
+		//	println(fmt.Sprintf("debug: tok: %v  state: %v", tok, state))
+		if tok == fflib.FFTok_error {
+			goto tokerror
+		}
+
+		switch state {
+
+		case fflib.FFParse_map_start:
+			if tok != fflib.FFTok_left_bracket {
+				wantedTok = fflib.FFTok_left_bracket
+				goto wrongtokenerror
+			}
+			state = fflib.FFParse_want_key
+			continue
+
+		case fflib.FFParse_after_value:
+			if tok == fflib.FFTok_comma {
+				state = fflib.FFParse_want_key
+			} else if tok == fflib.FFTok_right_bracket {
+				goto done
+			} else {
+				wantedTok = fflib.FFTok_comma
+				goto wrongtokenerror
+			}
+
+		case fflib.FFParse_want_key:
+			// json {} ended. goto exit. woo.
+			if tok == fflib.FFTok_right_bracket {
+				goto done
+			}
+			if tok != fflib.FFTok_string {
+				wantedTok = fflib.FFTok_string
+				goto wrongtokenerror
+			}
+
+			kn := fs.Output.Bytes()
+			if len(kn) <= 0 {
+				// "" case. hrm.
+				currentKey = ffj_t_sessionuserno_such_key
+				state = fflib.FFParse_want_colon
+				goto mainparse
+			} else {
+				switch kn[0] {
+
+				case 'f':
+
+					if bytes.Equal(ffj_key_sessionuser_Features, kn) {
+						currentKey = ffj_t_sessionuser_Features
+						state = fflib.FFParse_want_colon
+						goto mainparse
+					}
+
+				case 'u':
+
+					if bytes.Equal(ffj_key_sessionuser_UserId, kn) {
+						currentKey = ffj_t_sessionuser_UserId
+						state = fflib.FFParse_want_colon
+						goto mainparse
+
+					} else if bytes.Equal(ffj_key_sessionuser_Username, kn) {
+						currentKey = ffj_t_sessionuser_Username
+						state = fflib.FFParse_want_colon
+						goto mainparse
+					}
+
+				}
+				currentKey = ffj_t_sessionuserno_such_key
+				state = fflib.FFParse_want_colon
+				goto mainparse
+			}
+
+		case fflib.FFParse_want_colon:
+			if tok != fflib.FFTok_colon {
+				wantedTok = fflib.FFTok_colon
+				goto wrongtokenerror
+			}
+			state = fflib.FFParse_want_value
+			continue
+		case fflib.FFParse_want_value:
+
+			if tok == fflib.FFTok_left_brace || tok == fflib.FFTok_left_bracket || tok == fflib.FFTok_integer || tok == fflib.FFTok_double || tok == fflib.FFTok_string || tok == fflib.FFTok_bool || tok == fflib.FFTok_null {
+				switch currentKey {
+
+				case ffj_t_sessionuser_Features:
+					goto handle_Features
+
+				case ffj_t_sessionuser_UserId:
+					goto handle_UserId
+
+				case ffj_t_sessionuser_Username:
+					goto handle_Username
+
+				case ffj_t_sessionuserno_such_key:
+					err = fs.SkipField(tok)
+					if err != nil {
+						return fs.WrapErr(err)
+					}
+					state = fflib.FFParse_after_value
+					goto mainparse
+				}
+			} else {
+				goto wantedvalue
+			}
+		}
+	}
