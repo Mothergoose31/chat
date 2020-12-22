9a47768f8cb07ee994b2c39ba943832c2a031233
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2020-12-22 13:53:41 -0800

    namescache -ffjson

---
+++ chatFolder/backend/nameschache_ffjson.go
@@ -0,0 +1,17 @@
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	fflib "github.com/pquerna/ffjson/fflib/v1"
+)
+
+func (mj *NamesOut) MarshalJSON() ([]byte, error) {
+	var buf fflib.Buffer
+	buf.Grow(128)
+	err := mj.MarshalJSONBuf(&buf)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
